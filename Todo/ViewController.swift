f50a7be0a955893c24dbf303659159ff1a4904e9
zhm <music_zhm@163.com>
2019-08-13 21:39:16 +0800

    first commit

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    let itemArray = ["find out", "listen music", "buy eggs"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "TodoCell")
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        let row = tableView.cellForRow(at: indexPath)
+        row?.accessoryType = (row?.accessoryType == .checkmark) ? .none : .checkmark
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
 
 
 }
